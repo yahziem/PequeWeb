a5a39d8389cc2e745b88dbe4982d3d3a425a5158
Yahzi Emiliano Martínez García <77820643+yahziem@users.noreply.github.com>
2024-04-10 21:59:57 -0600

    Add files via upload

--- Gl/JS/script.js
+++ Gl/JS/script.js
@@ -2,39 +2,27 @@ const galleryContainer = document.getElementById('galleryContainer');
 
 // Puedes reemplazar estas URL con las de tus propias imágenes
 const imageUrls = [
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/1.jfif',
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/2.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/3.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/4.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/5.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/6.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/7.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/8.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/9.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/10.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/11.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/12.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/13.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/14.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/15.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/16.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/17.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/18.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/19.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/20.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/21.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/22.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/23.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/24.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/25.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/26.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/27.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/28.jfif',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/29.jfif ',    
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/30.jfif ', 
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/31.jfif ', 
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/32.jfif ', 
-    'https://raw.githubusercontent.com/yahziem/PequeWeb/main/Gl/fotos/33.jfif ', 
+    'https://yahzi.000webhostapp.com/Gl/fotos/1.jfif',
+    'https://yahzi.000webhostapp.com/Gl/fotos/2.jfif',
+    'https://yahzi.000webhostapp.com/Gl/fotos/3.jfif',
+    'https://yahzi.000webhostapp.com/Gl/fotos/4.jfif',
+    'https://yahzi.000webhostapp.com/Gl/fotos/5.jfif',
+    'https://yahzi.000webhostapp.com/Gl/fotos/6.jfif',
+    'https://yahzi.000webhostapp.com/Gl/fotos/7.jfif',
+    'https://yahzi.000webhostapp.com/Gl/fotos/8.jfif',
+    'https://yahzi.000webhostapp.com/Gl/fotos/9.jfif',
+    'https://yahzi.000webhostapp.com/Gl/fotos/10.jfif',
+    'https://yahzi.000webhostapp.com/Gl/fotos/11.jfif',
+    'https://yahzi.000webhostapp.com/Gl/fotos/12.jfif',
+    'https://yahzi.000webhostapp.com/Gl/fotos/13.jfif',
+    'https://yahzi.000webhostapp.com/Gl/fotos/14.jfif',
+    'https://yahzi.000webhostapp.com/Gl/fotos/15.jfif',
+    'https://yahzi.000webhostapp.com/Gl/fotos/16.jfif',
+    'https://yahzi.000webhostapp.com/Gl/fotos/17.jfif',
+    'https://yahzi.000webhostapp.com/Gl/fotos/18.jfif',
+    'https://yahzi.000webhostapp.com/Gl/fotos/19.jfif',
+    'https://yahzi.000webhostapp.com/Gl/fotos/20.jfif',
+    'https://yahzi.000webhostapp.com/Gl/fotos/21.jfif',
     // Agrega más URL de imágenes según sea necesario
 ];
 
@@ -48,6 +36,11 @@ function createGalleryItem(url) {
     const img = document.createElement('img');
     img.src = url;
     img.alt = url; // Cambiado para mostrar la URL de la imagen en caso de que el enlace esté roto
+
+    // Establecer un tamaño fijo para todas las imágenes
+    img.style.width = '300px';
+    img.style.height = '300px';
+    img.style.objectFit = 'cover'; // Ajuste para que la imagen se ajuste correctamente al tamaño definido
 
     // Agregar evento de clic para agrandar la imagen
     imgContainer.addEventListener('click', () => {

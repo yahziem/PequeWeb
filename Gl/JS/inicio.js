efbdb23768809734725258208c8df0208122babc
yahziem <yahziga@hotmail.com>
2024-12-07 00:04:52 -0600

    .add gogle anty

--- Gl/JS/inicio.js
+++ Gl/JS/inicio.js
@@ -52,3 +52,26 @@ document.addEventListener('click', (e) => {
         closeImage(enlargedImg);
     }
 });
+  // Comprobar si ya se ha dado el consentimiento
+  window.onload = function() {
+    if (!localStorage.getItem('cookies-accepted')) {
+      document.getElementById('cookie-banner').style.display = 'block';
+    } else {
+      document.getElementById('cookie-banner').style.display = 'none';
+    }
+  };
+
+  // Función para aceptar las cookies
+  document.getElementById('accept-cookies').addEventListener('click', function() {
+    localStorage.setItem('cookies-accepted', 'true'); // Guardar la preferencia
+    document.getElementById('cookie-banner').style.display = 'none'; // Ocultar el banner
+    // Aquí se puede cargar Google Analytics si es aceptado
+    gtag('config', 'G-V3TYXL31NM');
+  });
+
+  // Función para rechazar las cookies
+  document.getElementById('decline-cookies').addEventListener('click', function() {
+    localStorage.setItem('cookies-accepted', 'false'); // Guardar la preferencia
+    document.getElementById('cookie-banner').style.display = 'none'; // Ocultar el banner
+    // Aquí puedes bloquear la carga de Google Analytics si se rechaza
+  });
